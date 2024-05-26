78f87d71b55c222b11db2a922883eee9b076e702
Taher Mahmood <taher.ccg@gmail.com>
2024-05-26 00:38:28 +0000

    Add datepicker in js

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,22 @@
+/*
+    jQuery for MaterializeCSS initialization
+*/
+
 $(document).ready(function () {
     // Navbar collapse
-    $(".sidenav").sidenav({edge: "right"});
+    $(".sidenav").sidenav({
+        edge: "right"
+    });
     // Task List collapsible
     $('.collapsible').collapsible();
     // Urgent tooltip
     $('.tooltipped').tooltip();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
